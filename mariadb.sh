eba19f590f9d5099e09b00938944229b9cef6897
rukechen <rukechen@qnap.com>
2018-01-12 09:27:27 +0000

    Revise consul register mechanism to get default route instead of hard code.

--- mariadb.sh
+++ mariadb.sh
@@ -49,9 +49,11 @@ done
 
 sv start /etc/service/qobserviumweb
 tmp="$(hostname -I | sed 's/ *$//g')"
+defaultroute="$(ip route | grep 'default via' | awk '{print $3}')"
 export tmp=$tmp
+export DEFAULTROUTE=$defaultroute
 echo "Register service to consul..."
-curl -H "Content-Type:application/json" -X PUT -d '{"ID": "tmpID", "Name": "tmpName", "Address": "'"$tmp"'", "Port": 4504, "Check":{"DeregisterCriticalServiceAfter":"2m", "HTTP":"http://'"$tmp"':4504/api/about", "Interval":"10s"}}' http://10.0.3.1:8500/v1/agent/service/register
+curl -H "Content-Type:application/json" -X PUT -d '{"ID": "tmpID", "Name": "tmpName", "Address": "'"$tmp"'", "Port": 4504, "Check":{"DeregisterCriticalServiceAfter":"2m", "HTTP":"http://'"$tmp"':4504/api/about", "Interval":"10s"}}' http://"$DEFAULTROUTE":8500/v1/agent/service/register
 
 echo "Starting MariaDB..."
 /usr/bin/mysqld_safe --skip-syslog --datadir='/config/databases'
